d0176d04756dd0ad881f3c1e70b9a6152c5f5243
DeX-Coder <dehimangshu03@gmail.com>
2023-03-03 15:25:01 +0530

    Visual Changes
    ⋄ What's New
    ⇨ Enhanced color suitability.
    
    ⇨ Showing contact details in a card.
    
    ⇨ Added hover effects.
    
    ⇨ Updated settings.
    
    ⇨ Added a new view logic.
    
    ⇨ Added a route of respective view logic.
    
    ⇨ HTML optimizations.
    
    ⌘ Dev Zone:
    ⋄ You can customize on your own and leave a PR. The best customization will be merged!

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, details
 urlpatterns = [
     # all routes here.
     path('', home, name='home'),
+    path('<int:id>/', details, name='details')
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -4,31 +4,49 @@ from django.shortcuts import render
 # Create your views here.
 all_data = [
     {
+        "id": 1,
         "first_name": "De",
         "last_name": "X",
+        "image": "person_avatar.jpeg",
         "nickname": "",
         "birthday": date(2005, 3, 10),
         "address": "Some random address",
-        "contact": "9153552447"
+        "contact": {"Mobile": "9153552447"}
     },
     {
+        "id": 2,
         "first_name": "He",
         "last_name": "X",
+        "image":"person_avatar.jpeg",
         "nickname": "Hexagon",
         "birthday": date(2006, 3, 10),
         "address": "Some other address",
-        "contact": "6296538947"
+        "contact": {"Work":"6296538947", "Mobile":"9454785960"}
     },
     {
+        "id": 3,
         "first_name": "Te",
         "last_name": "X",
+        "image":"person_avatar.jpeg",
         "nickname": "Texagon",
         "birthday": date(2007, 3, 10),
         "address": "Some another address",
-        "contact": "6296539548"
+        "contact": {"Mobile":"6296539548"}
     }
 ]
 
 
 def home(request):
     return render(request, 'contacts/home.html', {'data': all_data, })
+
+
+def details(request, id):
+    particular_detail = all_data[id-1]
+    return render(request, 'contacts/details.html', {
+        "title":f"{particular_detail['first_name']} {particular_detail['last_name']}",
+        "contact":particular_detail['contact'],
+        "address":particular_detail['address'] or None,
+        "profile_image":particular_detail['image'] or None,
+        "birthday":particular_detail['birthday'] or None,
+        "nickname": particular_detail['nickname'] or None,
+    })
